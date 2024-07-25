9dd67f84d0c967b28165e91dd71a65fdc5630376
sharyndoolan <sdoolan@propharma.com.au>
2024-07-25 10:03:04 +1000

    formatting ready for buttons

--- src/components/BioButton.jsx
+++ src/components/BioButton.jsx
@@ -1,3 +1,5 @@
+import data from '///home/sharyndoolan/GA/unit3/homework/Geelong.src.players.json'
+
 const BioButton = () => {
   return <button>Bio</button>
 }
--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -4,11 +4,19 @@ import BioButton from './BioButton'
 const Players = (props) => {
   return (
     <div class="card">
+      <div class="number">
+        <p>{props.player.number}</p>
+      </div>
+      <div class="name">
+        <p>{props.player.name}</p>
+      </div>
       <img src={props.player.img} alt="Image" />
-      <p>Number: {props.player.number}</p>
-      <p>Name: {props.player.name}</p>
-      <p>Age: {props.player.age}</p>
-      <p>Games: {props.player.games}</p>
+      <div class="age">
+        <p>Age: {props.player.age}</p>
+      </div>
+      <div class="games">
+        <p>Games: {props.player.games}</p>
+      </div>
       <StatsButton />
       <BioButton />
     </div>
