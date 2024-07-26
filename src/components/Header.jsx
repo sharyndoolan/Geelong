e10b1f867eda5b58246e4697b2f637a1977231d9
sharyndoolan <sdoolan@propharma.com.au>
2024-07-27 06:37:56 +1000

    header but now stuffed up cards

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,34 @@
-const Head = () => {
-  return
-  ;<head>
-    <p>
-      It is believed that the co-founder of Australian football, Thomas
-      Wentworth Wills, recommended the formation of the Geelong Football Club in
-      July 1859 making it the second oldest continuously existing club at an
-      elite level of any code in the world. <br />
-      It is believed that rough and tough diggers on the Victorian goldfields
-      played football with no formal rules in the early 1850s. They came from
-      all corners of the world, including Wales, England, Scotland, Ireland,
-      France, Germany, America and China. They would conjure the best rules
-      available, anything they were familiar with; whatever they used it
-      produced a game that was fierce and chaotic, many a tussle settled with
-      strength and pluck and with 20 or more aside, flying boots, fists and
-      elbows produced bone shattering results. <br /> Geelong has a proud
-      history, having won ten AFL and seven VFA premierships in its history. The
-      Cats tasted success in 1925, 1931, 1937, 1951, 1952, 1963, 2007, 2009,
-      2011 and 2022. The club has also won flags at other levels, including the
-      night cup, reserves, under 19 and VFA/VFL. <BR></BR> Below are the players
-      of the 2024 squad.
-    </p>
-  </head>
+const Header = () => {
+  return (
+    <div>
+      <img
+        className="headerimg"
+        src="https://content.api.news/v3/images/bin/5024cceaa04695dde11f00378e65d3d9?width=768"
+        alt="2022 Premiership"
+      />
+      <br />
+      <h2>The Geelong Football Club</h2>
+      <p>
+        It is believed that the co-founder of Australian football, Thomas
+        Wentworth Wills, recommended the formation of the Geelong Football Club
+        in July 1859 making it the second oldest continuously existing club at
+        an elite level of any code in the world. <br />
+        It is believed that rough and tough diggers on the Victorian goldfields
+        played football with no formal rules in the early 1850s. They came from
+        all corners of the world, including Wales, England, Scotland, Ireland,
+        France, Germany, America and China. They would conjure the best rules
+        available, anything they were familiar with; whatever they used it
+        produced a game that was fierce and chaotic, many a tussle settled with
+        strength and pluck and with 20 or more aside, flying boots, fists and
+        elbows produced bone shattering results. <br /> Geelong has a proud
+        history, having won ten AFL and seven VFA premierships in its history.
+        The Cats tasted success in 1925, 1931, 1937, 1951, 1952, 1963, 2007,
+        2009, 2011 and 2022. The club has also won flags at other levels,
+        including the night cup, reserves, under 19 and VFA/VFL. <br /> Below
+        are the players of the 2024 squad.
+      </p>
+    </div>
+  )
 }
 
-export default Head
+export default Header
--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,28 +1,33 @@
 import StatsButton from './StatsButton'
 import BioButton from './BioButton'
-import Head from './Header'
+import Header from './Header'
 
 const Players = (props) => {
   return (
-    <div className="card">
-      <div className="number">
-        <p>{props.player.number}</p>
+    <div className="page">
+      <div className="header">
+        <Header />
       </div>
-      <div className="name">
-        <p>{props.player.name}</p>
-      </div>
-      <img src={props.player.img} alt="Image" />
-      <div className="age">
-        <p>Age: {props.player.age}</p>
-      </div>
-      <div className="games">
-        <p>Games: {props.player.games}</p>
-      </div>
-      <div className="stats">
-        <StatsButton />
-      </div>
-      <div className="bio">
-        <BioButton />
+      <div className="card">
+        <div className="number">
+          <p>{props.player.number}</p>
+        </div>
+        <div className="name">
+          <p>{props.player.name}</p>
+        </div>
+        <img src={props.player.img} alt="Image" />
+        <div className="age">
+          <p>Age: {props.player.age}</p>
+        </div>
+        <div className="games">
+          <p>Games: {props.player.games}</p>
+        </div>
+        <div className="stats">
+          <StatsButton />
+        </div>
+        <div className="bio">
+          <BioButton />
+        </div>
       </div>
     </div>
   )
